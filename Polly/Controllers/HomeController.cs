f5f21d6c8519eea673e3fc0272bf479d40216277
LDecker <Decker702@yahoo.com>
2017-05-08 23:46:49 -0400

    Created landing page

--- Polly/Controllers/HomeController.cs
+++ Polly/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Polly.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Polly's Posies Inventory Management System";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Please call Admin if you have questions";
 
             return View();
         }
